371f1da7787aa7d3eb3b6f5421f9e73356c5f538
TheDjSponge <103638591+TheDjSponge@users.noreply.github.com>
2026-02-26 18:30:41 +0100

    Chapter 3 (#1)
    Added chapter 3 code

--- README.md
+++ README.md
@@ -28,7 +28,7 @@ uv run cli/keyword_search_cli.py search "furious"
 
 # Project chapters
 
-## Chapter 1.
+## Chapter 1: Preprocessing.
 In this chapter we implemented a pretty naive word-matching between the user query and movie titles.
 To avoid false positives regarding semantics, we filtered some meaningless stop-words and reduced
 words to their root form (stem). The general idea of this word matching is to project the user query
@@ -41,7 +41,7 @@ Run this part of the project with
 uv run cli/keyword_search_cli.py search "furious"
 ```
 
-## Chapter 2. 
+## Chapter 2: TF-IDF
 In this chapter we implemented the TF-IDF counting method. 
 
 TF in a document represents term frequency, the number
@@ -55,11 +55,41 @@ but a word that appears a lot in a document but is also in all the others is way
 
 We can run the cli commands of this chapter with 
 ```bash
-uv run cli/keyword_search_cli.py build
+uv run cli/keyword_search_cli.py build ## Mandatory to build the index
 
 uv run cli/keyword_search_cli.py tf doc_id term # Get TF score for a term in a doc
 
 uv run cli/keyword_search_cli.py idf term # Get IDF score over dataset for a term
 
 uv run cli/keyword_search_cli.py tfidf doc_id term # Get TF-IDF score for a term in a doc over all documents
+```
+
+## Chapter 3: Keyword search
+
+In this chapter, we implement Okapi BM25, which is a TF-IDF calculation that provides more stability.
+formulas are:
+$$BM25_{idf} = \frac{num\_docs - doc\_freq + 0.5}{doc\_freq + 0.5}$$
+$$BM25_{tf} = \frac{tf * (k1 + 1)}{tf + k1 * length\_norm}$$
+with 
+$$length\_norm = 1 - b + b * \frac{doc\_length}{avg\_doc\_length}$$
+
+In our implementation of TF-IDF, the TF weight scales lineraly with the appearances of a term, regardless of document length. It means that an excessively
+long document containing a unique word a lot of times would have a massive score.
+BM25 introduces saturation. More occurences of the same word in a document results in a progressively lower increase on score, 
+as well as document length normalization to mitigate that effect. Both term effects can be regulated through the k1 and b parameters
+that generally hold values of $k_1 \in [1.2,2.0]$ and $b=0.75$ (source https://fr.wikipedia.org/wiki/Okapi_BM25)
+
+Practically, a document with a million words that uses the word "car" a thousand times would give a better TF-IDF score
+than a document of 100 words where the word car appears 50, which is a surprising conclusion since document one would have 0.1% of car occurences 
+when document 2 would have 50%.
+
+we can run the cli commands of this chapter with
+```bash
+uv run cli/keyword_search_cli.py build ## Mandatory to build the index
+
+uv run cli/keyword_search_cli.py bm25idf grizzly
+
+uv run cli/keyword_search_cli.py bm25tf 1 anbuselvan
+
+uv run cli/keyword_search_cli.py bm25search "love story"
 ```
--- cli/tf_idf/inverted_index.py
+++ cli/tf_idf/inverted_index.py
@@ -4,6 +4,7 @@ import pickle
 from typing import List, Dict
 from collections import Counter
 from cli.utils.text_processing import prepare_and_tokenize
+from cli.utils.constants import CACHE_DIR, BM25_K1, BM25_B, RETRIEVAL_LIMIT
 
 
 class InvertedIndex:
@@ -11,12 +12,19 @@ class InvertedIndex:
         # Index is the mapping between token ID -> documents. Structured as "hello" : [0,10,20]
         # Where number are supposed to be document IDs
         self.index = {}
+        self.index_path = os.path.join(CACHE_DIR, "index.pkl")
 
         # Docmaps is the relation between document IDs (0,10,20) to the full document object {title + description}
         self.docmap = {}
+        self.docmap_path = os.path.join(CACHE_DIR, "docmap.pkl")
 
         # Term frequency holds for each document ID a counter of the words present in it.
         self.term_frequencies = {}
+        self.term_frequencies_path = os.path.join(CACHE_DIR, "term_frequencies.pkl")
+
+        # Stores length of each doc
+        self.doc_lengths = {}
+        self.doc_lengths_path = os.path.join(CACHE_DIR, "doc_lengths.pkl")
 
     def build(self, movies: List[Dict]) -> None:
         print("~ Building inverse index, document map and counting term frequencies ~")
@@ -30,15 +38,71 @@ class InvertedIndex:
         tokenized_term = prepare_and_tokenize(term)
         if len(tokenized_term) != 1:
             raise ValueError("term should contain a single token")
-        return self.term_frequencies[doc_id][tokenized_term[0]]
+        token = tokenized_term[0]
+        return self.term_frequencies[doc_id][token]
 
     def get_idf(self, term: str) -> float:
-        tokenized_term = prepare_and_tokenize(term)[0]
+        tokenized_term = prepare_and_tokenize(term)
+        if len(tokenized_term) != 1:
+            raise ValueError("term should contain a single token")
+        token = tokenized_term[0]
 
         total_doc_count = len(self.docmap)
-        term_match_doc_count = len(self.get_document(tokenized_term))
+        term_match_doc_count = len(self.get_document(token))
 
         return math.log((total_doc_count + 1) / (term_match_doc_count + 1))
+
+    def get_bm25_idf(self, term: str) -> float:
+        tokenized_term = prepare_and_tokenize(term)
+        if len(tokenized_term) != 1:
+            raise ValueError("term should contain a single token")
+        token = tokenized_term[0]
+
+        total_docs = len(self.docmap)
+        document_frequency = len(self.index[token])
+        return math.log(
+            (total_docs - document_frequency + 0.5) / (document_frequency + 0.5) + 1
+        )
+
+    def get_bm25_tf(
+        self, doc_id: int, term: str, k1: float = BM25_K1, b: float = BM25_B
+    ):
+        avg_doc_length = self.__get_avg_doc_length()
+        doc_length = self.doc_lengths.get(doc_id, 0)
+        if avg_doc_length > 0:
+            length_norm = 1 - b + b * (doc_length / avg_doc_length)
+        else:
+            length_norm = 1
+
+        tf = self.get_tf(doc_id, term)
+        bm25_tf = (tf * (k1 + 1)) / (tf + k1 * length_norm)
+
+        return bm25_tf
+
+    def bm25(self, doc_id: int, term: str):
+        bm25_tf = self.get_bm25_tf(doc_id, term)
+        bm25_idf = self.get_bm25_idf(term)
+        return bm25_tf * bm25_idf
+
+    def bm25_search(self, query: str, limit: int = RETRIEVAL_LIMIT):
+        tokenized_query = prepare_and_tokenize(query)
+        scores = {}
+        for token in tokenized_query:
+            for doc_id in self.index[token]:
+                if doc_id in scores:
+                    scores[doc_id] += self.bm25(doc_id=doc_id, term=token)
+                else:
+                    scores[doc_id] = self.bm25(doc_id=doc_id, term=token)
+
+        sorted_scores = dict(
+            sorted(scores.items(), key=lambda item: item[1], reverse=True)
+        )
+        matches = []
+        for doc_id, score in sorted_scores.items():
+            matches.append(dict(doc_id=doc_id, bm25_score=score))
+            if len(matches) >= limit:
+                break
+        return matches
 
     def get_document(self, term: str) -> list[int]:
         return sorted(self.index.get(term.lower(), []))
@@ -47,32 +111,43 @@ class InvertedIndex:
         print(
             "~ Saving index.pkl, docmap.pkl and term_frequencies.pkl files under ./cache ~"
         )
-        if not os.path.exists("./cache"):
-            os.makedirs("./cache")
+        if not os.path.exists(CACHE_DIR):
+            os.makedirs(CACHE_DIR)
 
-        with open("./cache/index.pkl", "wb") as index:
+        with open(self.index_path, "wb") as index:
             pickle.dump(self.index, index)
 
-        with open("./cache/docmap.pkl", "wb") as docmap:
+        with open(self.docmap_path, "wb") as docmap:
             pickle.dump(self.docmap, docmap)
 
-        with open("./cache/term_frequencies.pkl", "wb") as tf:
+        with open(self.term_frequencies_path, "wb") as tf:
             pickle.dump(self.term_frequencies, tf)
 
-    def load(self) -> None:
-        if (not os.path.exists("./cache/index.pkl")) or (
-            not os.path.exists("./cache/docmap.pkl")
-        ):
-            raise FileNotFoundError("index.pkl or docmap.pkl not found under ./cache")
+        with open(self.doc_lengths_path, "wb") as doc_lengths:
+            pickle.dump(self.doc_lengths, doc_lengths)
 
-        with open("./cache/index.pkl", "rb") as index:
+    def load(self) -> None:
+        if (
+            (not os.path.exists(self.index_path))
+            or (not os.path.exists(self.docmap_path))
+            or (not os.path.exists(self.term_frequencies_path))
+            or (not os.path.exists(self.doc_lengths_path))
+        ):
+            raise FileNotFoundError(
+                "index.pkl, docmap.pkl, term_frequencies.pkl or doc_lengths.pkl not found under ./cache"
+            )
+
+        with open(self.index_path, "rb") as index:
             self.index = pickle.load(index)
 
-        with open("./cache/docmap.pkl", "rb") as docmap:
+        with open(self.docmap_path, "rb") as docmap:
             self.docmap = pickle.load(docmap)
 
-        with open("./cache/term_frequencies.pkl", "rb") as tf:
+        with open(self.term_frequencies_path, "rb") as tf:
             self.term_frequencies = pickle.load(tf)
+
+        with open(self.doc_lengths_path, "rb") as doc_lengths:
+            self.doc_lengths = pickle.load(doc_lengths)
 
     def __add_document(self, doc_id: int, text: str) -> None:
         tokenized_text = prepare_and_tokenize(text)
@@ -82,3 +157,11 @@ class InvertedIndex:
             else:
                 self.index[token] = [doc_id]
         self.term_frequencies[doc_id] = Counter(tokenized_text)
+        self.doc_lengths[doc_id] = len(tokenized_text)
+
+    def __get_avg_doc_length(self) -> float:
+        doc_token_counts = list(self.doc_lengths.values())
+        if len(doc_token_counts) == 0:
+            return 0.0
+        else:
+            return sum(doc_token_counts) / len(doc_token_counts)
